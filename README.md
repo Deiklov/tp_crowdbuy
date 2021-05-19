8d5c7728959c2d3c40cb7969008f74e8514bf8fd
Deiklov <romanov408g@mail.ru>
2021-05-19 10:03:39 +0300

    with readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # tp_crowdbuy
-Совместная покупка товара за эфир
+Выполнил Романов Андрей  
+Вариант: коллективная покупка товара за ETH с произвольных адресов  
+address in Rinkeby: 0x91e3A4CCd18640aAB4A79a6980563f9FD8c09F91  
+Инструкция по запуску:  
+1. docker build -t test_brownie .
+2. docker run test_brownie  
+Смотреть вывод консоли или вызвать docker logs  
+[YouTube Screencast link](https://youtu.be/lqoF9roDU9w)
--- scripts/deploy_testnet.py
+++ scripts/deploy_testnet.py
@@ -6,11 +6,14 @@ import os
 
 # my metamask address
 rinkeby_address = "0xb0Ea766e0D0160F6e97c5B0B8B7b63F34c783e8A"
-
+#  contract id 0x91e3A4CCd18640aAB4A79a6980563f9FD8c09F91
 
 # export PRIVATE_KEY=metamask key
 # export WEB3_INFURA_PROJECT_ID
 # brownie run scripts/deploy_testnet.py --network rinkeby
+# Верификация контракта
+#  ctrct=CrowdBuy.at("0x91e3A4CCd18640aAB4A79a6980563f9FD8c09F91")
+# CrowdBuy.publish_source(ctrct)
 
 def main():
     if network.show_active() == "rinkeby":
